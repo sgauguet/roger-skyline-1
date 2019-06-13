4334ec155150c65d19e0bed8a4c2fb3903e4636f
sgauguet <odinthorlokira@gmail.com>
2019-06-13 11:34:44 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -501,7 +501,7 @@ chmod +x /usr/local/sbin/crontab-updates
 
 echo -e \"${GREEN}Modification de la crontab\${RES}\"
 crontab -l > cron_list
-echo \"1 * * * * root /usr/local/sbin/crontab-updates\" >> cron_list
+echo \"*/1 * * * * /usr/local/sbin/crontab-updates\" >> cron_list
 crontab -u root cron_list
 rm -rf cron_list
 service cron start
