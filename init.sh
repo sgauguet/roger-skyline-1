6e21e9281160de9fbb7223b44ab8a452977c90c5
root <root@Simon.localdomain>
2019-06-19 14:45:34 +0200

    edit script

--- init.sh
+++ init.sh
@@ -518,24 +518,21 @@ chmod 755 \$WEB_DIR
 # cp /etc/nginx/sites-available/default /etc/nginx/sites-available/\$HOST_NAME.conf
 
 cat > /etc/nginx/sites-available/\$HOST_NAME.conf <<EOF
+
 server {
-    server_name *.\$HOST_NAME;
-    return 301 \\\$scheme://\$HOST_NAME\\\$request_uri;
-}
-server {
-    server_name \$HOST_NAME;
+    server_name \$HOST_NAME www.\$HOST_NAME;
 
 	location / {
 		root        /data/www/\$HOST_NAME/html;
-		try_files $uri $uri/ = 404;
+		try_files \$uri \$uri/ = 404;
 	}
 
-	location /css/ {
+	location ~* \.(css) {
 		root        /data/www/\$HOST_NAME/css;
-		try_files $uri $uri/ = 404;
+		try_files \$uri $uri/ = 404;
 	}
 
-	location /js/ {
+	location ~* \.(js) {
 		root        /data/www/\$HOST_NAME/js;
 		try_files $uri $uri/ = 404;
 	}
@@ -557,11 +554,11 @@ cat > \$WEB_DIR/\$HOST_NAME/html/index.html <<EOF
 <link href='index.css' rel='stylesheet' type='text/css'>
 <form method='post' action='success.html'>
 <div class='box'>
-<h1>Dashboard</h1>
+<h1>ROGER-SKYLINE-1</h1>
 <input type='email' name='email' value='email' class='email' />
 <input type='password' name='email' value='email' class='email' />
   
-<a href='#'><div class='btn'>Sign In</div></a> <!-- End Btn -->
+<a href='success.html'><div class='btn'>Sign In</div></a> <!-- End Btn -->
 
 <a href='#'><div id='btn2'>Sign Up</div></a> <!-- End Btn2 -->
   
@@ -573,6 +570,35 @@ cat > \$WEB_DIR/\$HOST_NAME/html/index.html <<EOF
   
 <script src='//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js' type='text/javascript'></script>
 <script src='index.js' type='text/javascript'></script>
+EOF
+
+cat > \$WEB_DIR/\$HOST_NAME/html/success.html <<EOF
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:700,600' rel='stylesheet' type='text/css'>
+<link href='index.css' rel='stylesheet' type='text/css'>
+<div class='box'>
+<h1>SUCCESS</h1>
+</div>
+  
+<script src='index.js' type='text/javascript'></script>
+EOF
+
+cat > \$WEB_DIR/\$HOST_NAME/html/error.html <<EOF
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:700,600' rel='stylesheet' type='text/css'>
+<link href='error.css' rel='stylesheet' type='text/css'>
+ <div id='clouds'>
+            <div class='cloud x1'></div>
+            <div class='cloud x1_5'></div>
+            <div class='cloud x3'></div>
+            <div class='cloud x4'></div>
+            <div class='cloud x5'></div>
+        </div>
+        <div class='c'>
+            <div class='_404'>404</div>
+            <hr>
+            <div class='_1'>THE PAGE</div>
+            <div class='_2'>WAS NOT FOUND</div>
+            <a class='btn' href='#'>BACK TO MARS</a>
+        </div>
 EOF
 
 cat > \$WEB_DIR/\$HOST_NAME/css/index.css <<EOF
@@ -668,6 +694,231 @@ background:#3594D2;
 }
 EOF
 
+cat > \$WEB_DIR/\$HOST_NAME/css/error.css <<EOF
+@import url(https://fonts.googleapis.com/css?family=opensans:500);
+body{
+                background: #33cc99;
+                color:#fff;
+                font-family: 'Open Sans', sans-serif;
+                max-height:700px;
+                overflow: hidden;
+            }
+            .c{
+                text-align: center;
+                display: block;
+                position: relative;
+                width:80%;
+                margin:100px auto;
+            }
+            ._404{
+                font-size: 220px;
+                position: relative;
+                display: inline-block;
+                z-index: 2;
+                height: 250px;
+                letter-spacing: 15px;
+            }
+            ._1{
+                text-align:center;
+                display:block;
+                position:relative;
+                letter-spacing: 12px;
+                font-size: 4em;
+                line-height: 80%;
+            }
+            ._2{
+                text-align:center;
+                display:block;
+                position: relative;
+                font-size: 20px;
+            }
+            .text{
+                font-size: 70px;
+                text-align: center;
+                position: relative;
+                display: inline-block;
+                margin: 19px 0px 0px 0px;
+                /* top: 256.301px; */
+                z-index: 3;
+                width: 100%;
+                line-height: 1.2em;
+                display: inline-block;
+            }
+           
+
+            .btn{
+                background-color: rgb( 255, 255, 255 );
+                position: relative;
+                display: inline-block;
+                width: 358px;
+                padding: 5px;
+                z-index: 5;
+                font-size: 25px;
+                margin:0 auto;
+                color:#33cc99;
+                text-decoration: none;
+                margin-right: 10px
+            }
+            .right{
+                float:right;
+                width:60%;
+            }
+            
+            hr{
+                padding: 0;
+                border: none;
+                border-top: 5px solid #fff;
+                color: #fff;
+                text-align: center;
+                margin: 0px auto;
+                width: 420px;
+                height:10px;
+                z-index: -10;
+            }
+            
+            hr:after {
+                content: "\2022";
+                display: inline-block;
+                position: relative;
+                top: -0.75em;
+                font-size: 2em;
+                padding: 0 0.2em;
+                background: #33cc99;
+            }
+            
+            .cloud {
+                width: 350px; height: 120px;
+
+                background: #FFF;
+                background: linear-gradient(top, #FFF 100%);
+                background: -webkit-linear-gradient(top, #FFF 100%);
+                background: -moz-linear-gradient(top, #FFF 100%);
+                background: -ms-linear-gradient(top, #FFF 100%);
+                background: -o-linear-gradient(top, #FFF 100%);
+
+                border-radius: 100px;
+                -webkit-border-radius: 100px;
+                -moz-border-radius: 100px;
+
+                position: absolute;
+                margin: 120px auto 20px;
+                z-index:-1;
+                transition: ease 1s;
+            }
+
+            .cloud:after, .cloud:before {
+                content: '';
+                position: absolute;
+                background: #FFF;
+                z-index: -1
+            }
+
+            .cloud:after {
+                width: 100px; height: 100px;
+                top: -50px; left: 50px;
+
+                border-radius: 100px;
+                -webkit-border-radius: 100px;
+                -moz-border-radius: 100px;
+            }
+
+            .cloud:before {
+                width: 180px; height: 180px;
+                top: -90px; right: 50px;
+
+                border-radius: 200px;
+                -webkit-border-radius: 200px;
+                -moz-border-radius: 200px;
+            }
+            
+            .x1 {
+                top:-50px;
+                left:100px;
+                -webkit-transform: scale(0.3);
+                -moz-transform: scale(0.3);
+                transform: scale(0.3);
+                opacity: 0.9;
+                -webkit-animation: moveclouds 15s linear infinite;
+                -moz-animation: moveclouds 15s linear infinite;
+                -o-animation: moveclouds 15s linear infinite;
+            }
+            
+            .x1_5{
+                top:-80px;
+                left:250px;
+                -webkit-transform: scale(0.3);
+                -moz-transform: scale(0.3);
+                transform: scale(0.3);
+                -webkit-animation: moveclouds 17s linear infinite;
+                -moz-animation: moveclouds 17s linear infinite;
+                -o-animation: moveclouds 17s linear infinite; 
+            }
+
+            .x2 {
+                left: 250px;
+                top:30px;
+                -webkit-transform: scale(0.6);
+                -moz-transform: scale(0.6);
+                transform: scale(0.6);
+                opacity: 0.6; 
+                -webkit-animation: moveclouds 25s linear infinite;
+                -moz-animation: moveclouds 25s linear infinite;
+                -o-animation: moveclouds 25s linear infinite;
+            }
+
+            .x3 {
+                left: 250px; bottom: -70px;
+
+                -webkit-transform: scale(0.6);
+                -moz-transform: scale(0.6);
+                transform: scale(0.6);
+                opacity: 0.8; 
+
+                -webkit-animation: moveclouds 25s linear infinite;
+                -moz-animation: moveclouds 25s linear infinite;
+                -o-animation: moveclouds 25s linear infinite;
+            }
+
+            .x4 {
+                left: 470px; botttom: 20px;
+
+                -webkit-transform: scale(0.75);
+                -moz-transform: scale(0.75);
+                transform: scale(0.75);
+                opacity: 0.75;
+
+                -webkit-animation: moveclouds 18s linear infinite;
+                -moz-animation: moveclouds 18s linear infinite;
+                -o-animation: moveclouds 18s linear infinite;
+            }
+
+            .x5 {
+                left: 200px; top: 300px;
+
+                -webkit-transform: scale(0.5);
+                -moz-transform: scale(0.5);
+                transform: scale(0.5);
+                opacity: 0.8; 
+
+                -webkit-animation: moveclouds 20s linear infinite;
+                -moz-animation: moveclouds 20s linear infinite;
+                -o-animation: moveclouds 20s linear infinite;
+            }
+
+            @-webkit-keyframes moveclouds {
+                0% {margin-left: 1000px;}
+                100% {margin-left: -1000px;}
+            }
+            @-moz-keyframes moveclouds {
+                0% {margin-left: 1000px;}
+                100% {margin-left: -1000px;}
+            }
+            @-o-keyframes moveclouds {
+                0% {margin-left: 1000px;}
+                100% {margin-left: -1000px;}
+            }
+EOF
+
 
 /usr/local/nginx/sbin/nginx -t 2>/dev/null > /dev/null
 if [[ $? == 0 ]]; then
@@ -701,8 +952,13 @@ function field_focus(field, email)
 
 //Stop click event
 \\\$('a').click(function(event){
-    event.preventDefault(); 
+    window.location.href='success.html';
 });
+
+\\\$('u').click(function(event){
+    window.location.href='error.html';
+});
+
 EOF
 
 " > $DIRECTORY/deployment.sh
