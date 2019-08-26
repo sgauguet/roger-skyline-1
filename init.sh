b66ad9632b1a370e5dc2f65fbb99dcaaf6ba879f
sgauguet <odinthorlokira@gmail.com>
2019-08-26 09:25:25 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -204,7 +204,7 @@ service sshd restart
 # Mise en place de l'acces par publickeys
 
 echo -e \"\${GREEN}Publikeys SSH\${RES}\"
-cp $DIRECTORY/publickey ~/.ssh/authorized_keys
+cp $DIRECTORY/publickey /home/\$USER/.ssh/authorized_keys
 #ssh-keygen -t rsa -f /home/\$USER/.ssh/id_rsa -P \"\"
 #ssh-copy-id -f -i /home/\$USER/.ssh/id_rsa.pub -p \$PORT_SSH \$USER@\$IP
 
