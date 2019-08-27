6af9bf1c490aa3435a6380e50907f3cd43830b65
sgauguet <odinthorlokira@gmail.com>
2019-08-27 11:10:18 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -135,6 +135,7 @@ then
 	curl -fsSL https://nginx.org/keys/nginx_signing.key | apt-key add -
 	apt-key fingerprint ABF5BD827BD9BF62
 	apt update
+	apt install nginx
 	apt-get install nginx-common
 fi
 
