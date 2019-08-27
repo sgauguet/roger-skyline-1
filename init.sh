2931bcb0a0a7b6573a1e54084b89c551ff5f637d
sgauguet <odinthorlokira@gmail.com>
2019-08-27 10:01:03 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -130,8 +130,8 @@ install vim git sudo net-tools fail2ban nmap ssh openssh-server iptables-persist
 
 if [ ! -f /etc/apt/sources.list.d/nginx.list ]
 then
-	echo \"deb http://nginx.org/packages/mainline/debian/ `lsb_release -cs` nginx
-	deb-src http://nginx.org/packages/mainline/debian/ stretch nginx\" > /etc/apt/sources.list.d/nginx.list
+	echo \"deb http://nginx.org/packages/debian/ `lsb_release -cs` nginx
+	deb-src http://nginx.org/packages/debian/ stretch nginx\" > /etc/apt/sources.list.d/nginx.list
 	curl -fsSL https://nginx.org/keys/nginx_signing.key | apt-key add -
 	apt-key fingerprint ABF5BD827BD9BF62
 	apt update
