9ea9b2fe9346d4c04aa488a0a1c28bf7af7c758b
sgauguet <odinthorlokira@gmail.com>
2019-09-16 10:32:51 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -243,7 +243,7 @@ service sshd restart
 # Test de la configuration SSH
 
 echo -e \"\${GREEN}Test de la nouvelle configuration\${RES}\"
-if [ \$(nmap -A -p 59112 --open 10.177.42.221 | grep -c open ) -eq 0 ]
+if [ \$(nmap -A -p 59112 --open 10.177.42.221 | grep -c open ) -eq 0 && \$(nmap -A -p 59112 --open \$IP_B | grep -c open ) -eq 0]
 then
 	echo -e \"\${RED}Echec\$RES\"
 	exit 1;
